2f854a49fed33574494c1320ee123821890b2396
RubensCarvalhoRocha <rocharubens4@gmail.com>
2023-09-15 09:42:27 -0300

    Limpar as palavras e armazena-las em uma lista

--- src/utils/ColetarPalavrasLimpas.java
+++ src/utils/ColetarPalavrasLimpas.java
@@ -4,6 +4,7 @@
  */
 package utils;
 
+import java.awt.List;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -58,21 +59,27 @@ public ArrayList<String> coletarPalavras() {
             System.out.println("Nenhum arquivo selecionado");
         }
 
-        return palavras;
-    }
-
-     public static void main(String[] args) throws IOException {
-        ColetarStopWords coletarStopWords = new ColetarStopWords();
-        Set<String> stopWords = coletarStopWords.coletarStopWords();
+        return palavras;   
         
-      for (String palavra : stopWords) {
-        System.out.println(palavra);
-        }
     }
-     
+
+
+   // Método que recebe uma lista de palavras e um conjunto de stop words e retorna uma lista de palavras limpas.
+public ArrayList<String> limparPalavras(ArrayList<String> palavras, Set<String> stopWords) {
+    // Cria uma nova ArrayList para armazenar as palavras limpas.
+    ArrayList<String> palavrasLimpas = new ArrayList<>();
+    
+    // Itera através de cada palavra na lista de entrada 'palavras'.
+    for (String palavra : palavras) {
+        // Verifica se a palavra não está presente no conjunto de stop words antes de adicioná-la às palavras limpas.
+            if (!stopWords.contains(palavra)) {
+            // Se a palavra não estiver nas stop words, ela é adicionada à lista de palavras limpas.
+            palavrasLimpas.add(palavra);
+            }
+        }
+    
+    // Retorna a lista de palavras limpas após o processamento.
+    return palavrasLimpas;
+    
+    }
 }
-
-
-
-
-
