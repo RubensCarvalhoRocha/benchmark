fe9f8dfd170044e8b4b96971607273db97405f1d
RubensCarvalhoRocha <rocharubens4@gmail.com>
2023-09-20 17:26:50 -0300

    implementando a busca Binaria 2;0

--- src/estruturas/BuscaBinaria2.java
+++ src/estruturas/BuscaBinaria2.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package estruturas;
 
-/**
- *
- * @author Administrator
- */
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+import bo.Coleta;
+
 public class BuscaBinaria2 {
+ 
+     public static void main() {
+        // Criando um ArrayList de inteiros
+        ArrayList<String> vetorDinamico = new ArrayList<>();
+        
+        Coleta coleta = new Coleta();
+        
+        try {
+            // Chamar o método coletarStopWords
+            Set<String> stopWords = coleta.coletarStopWords();
+            // Chamar o método coletarPalavras
+            ArrayList<String> palavras = coleta.coletarPalavras();
+            // Chamar o método removerStopWords, passando as palavras coletadas e as stop words como argumentos
+            ArrayList<String> palavrasLimpas = coleta.removerStopWords(palavras, stopWords);
+
+             for (String palavra : palavrasLimpas) {
+                if (!vetorDinamico.contains(palavra)) {
+                    // A palavra não foi encontrada no vetor dinâmico, então a adicionamos
+                    vetorDinamico.add(palavra);
+                }
+            }
+
+        } catch (IOException e) {}
+        
+     }
+     
+      public static int buscaBinaria(ArrayList<String> listaOrdenada, String alvo) {
+        int esquerda = 0;
+        int direita = listaOrdenada.size() - 1;
+
+        while (esquerda <= direita) {
+            int meio = esquerda + (direita - esquerda) / 2;
+            int comparacao = alvo.compareTo(listaOrdenada.get(meio));
+
+            if (comparacao == 0) {
+                return meio; // Encontrou a palavra
+            } else if (comparacao < 0) {
+                direita = meio - 1; // A palavra está à esquerda
+            } else {
+                esquerda = meio + 1; // A palavra está à direita
+            }
+        }
+
+        return -1; // A palavra não foi encontrada
+    }
     
 }
